11bab5ed4267b79b8f5eccf15642808ab09eea48
shambhavi2177 <shambhavi2177@gmail.com>
2026-01-16 21:56:38 +0530

    bug(auth): add auth/me route

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -5,8 +5,19 @@ import {
   registerValidator,
   loginValidator,
 } from "../validators/auth.validator.js";
+import authMiddleware from "../middleware/auth.middleware.js";
 
 const router = Router();
+
+router.get("/me",authMiddleware, (req, res) => {
+  res.status(200).json({
+    user: {
+      id: req.user.id,
+      email: req.user.email
+    }
+  });
+});
+
 
 /* REGISTER */
 router.post("/register", authLimiter, registerValidator, register);
